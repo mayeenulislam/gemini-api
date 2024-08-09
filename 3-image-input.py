3d4fad80b9ed99b2345c59af4ba196a95d78d612
Mayeenul Islam <wz.islam@gmail.com>
2024-08-10 00:58:03 +0600

    Fix the Image Model

--- 3-image-input.py
+++ 3-image-input.py
@@ -15,15 +15,15 @@ import PIL
 
 image = PIL.Image.open('assets/sample-image.jpg')
 # vision_model = genai.GenerativeModel('gemini-pro-vision') # deprecated since 12 July 2024
-vision_model = genai.GenerativeModel('gemini-1.5-flash')
+vision_model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
 # Explain the image
-# response = vision_model.generate_content(["Explain the picture", image])
+response = vision_model.generate_content(["Explain the picture", image])
 
 # Generate a story based on the image
 # response = vision_model.generate_content(["Write a story from the picture and set a title with suspected place where the image was taken", image])
 
 # Count the objects from the image and response in JSON format
-response = vision_model.generate_content(["Generate a json of ingredients with their count visible in the image", image])
+# response = vision_model.generate_content(["Generate a json of ingredients with their count visible in the image", image])
 
 print(response.text)
